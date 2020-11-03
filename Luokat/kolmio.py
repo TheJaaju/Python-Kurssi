efd42d4a71b78202f22d1cd11d7fa51f3401de26
TheJaaju <62209959+TheJaaju@users.noreply.github.com>
2020-11-03 14:05:29 +0200

    Add files via upload

--- Luokat/kolmio.py
+++ Luokat/kolmio.py
@@ -3,15 +3,14 @@
 class Piirto():
 
     def __init__(self):
-        self.height = 30
-        self.height2 = self.height*2+1
+        self.width = 30
         self. char1 = "#"
         self.char2 = " "
 
     def drawIt(self):
-        for i in range(1,self.height,2):
-            self.height = self.height - 1
-            print(self.char2*self.height + self.char1*i)
+        for i in range(1,self.width,2):
+            self.width = self.width - 1
+            print(self.char2*self.width + self.char1*i)
 
 
 draw = Piirto()
