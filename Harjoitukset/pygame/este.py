ed25c0fa2f135f3278d6f0aaaa4c49f96864ede1
TheJaaju <62209959+TheJaaju@users.noreply.github.com>
2020-11-09 13:29:18 +0200

    Create este.py

---
+++ Harjoitukset/pygame/este.py
@@ -0,0 +1,12 @@
+#Liikkuvat esteet
+
+import pygame(
+
+pygame.init()
+
+window = (500,600)
+title = "Liikkuvia esteitä"
+
+screen = pygame.display.set_mode(window)
+pygame.display.set_caption(title)
+
