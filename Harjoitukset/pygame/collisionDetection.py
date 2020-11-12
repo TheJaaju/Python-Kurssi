01c85f3f319e20d66de261ddbed1d18f05ea1cad
TheJaaju <62209959+TheJaaju@users.noreply.github.com>
2020-11-12 09:18:50 +0200

    Create collisionDetection.py

---
+++ Harjoitukset/pygame/collisionDetection.py
@@ -0,0 +1,138 @@
+#Reunukset kentälle
+# +Liikkuvagt pallot
+# +Törmäykset
+
+import pygame
+import math
+
+from pygame import draw
+
+pygame.init()
+Running = True
+#gameDisplay Settings
+resolution = 1280,720
+title = "Borders"
+
+gameDisplay = pygame.display.set_mode(resolution)
+pygame.display.set_caption(title)
+
+#Canvas
+def drawSurfRect(rectWidth,rectHeight):
+    #border = pygame.Rect((rectWidth,rectHeight))
+    borderSurface = pygame.Surface((rectWidth,rectHeight))
+    borderSurface.fill((200,100,12))
+    return borderSurface
+
+class Ball:
+    def __init__(self, posx, posy, velx, vely, r):
+        self.position = [posx, posy]
+        self.speedMagnitude = 0.5
+        self.dirx, self.diry = velx, vely
+
+        self.r = r
+
+        #define unitvector
+        self.length = math.sqrt(velx*velx + vely*vely)
+        self.unitVector = [velx / self.length, vely / self.length]
+
+        self.speed = [0,0]
+    
+    def calcSpeed(self):
+        self.speed[0] = self.unitVector[0]*self.speedMagnitude
+        self.speed[1] = self. unitVector[1]*self.speedMagnitude
+
+    def displayBall(self):
+        pygame.draw.circle(gameDisplay, (200,0,200), (int(self.position[0]),int(self.position[1])),self.r)
+
+    def moveBall(self):
+        self.position[0] += self.speed[0]
+        self.position[1] += self.speed[1]
+
+borderTop = drawSurfRect(1280,10)
+borderBottom = drawSurfRect(1280,10)
+borderLeft = drawSurfRect(10,720)
+borderRight = drawSurfRect(10,720)
+
+#borderList = [borderTop,borderBottom,borderLeft, borderRight]
+
+topX, topY = 0,0
+bottomX, bottomY = 0,710
+leftX, leftY = 0,0
+rightX, rightY = 1270,0
+
+ball = Ball(200,400,1,-1,20)
+ball2 = Ball(300,200,-1,1,10)
+ball3 = Ball(100,260,1,1,5)
+ball4 = Ball(350,123,0.5,1,20)
+
+balls = [ball,ball2,ball3,ball4]
+
+def checkBallCollision(ball1, ball2):
+    bdistance = math.hypot(ball1.position[0] - ball2.position[1], ball1.position[1] - ball2.position[1])
+    unit1x = ball1.unitVector[0]
+    unit1y = ball.unitVector[1]
+    unit2x = ball2.unitVector[0]
+    unit2y = ball2.unitVector[1]
+
+    collisionDistance = ball1.r + ball2.r
+
+    if (bdistance <= collisionDistance):
+        ball1.unitVector[0] = unit2x
+        ball1.unitVector[1] = unit2y
+        ball2.unitVector[0] = unit1x
+        ball2.unitVector[1] = unit1y
+
+
+def checkBorderCollision(position,borderpos,collsionDistance):
+    if(abs(position - borderpos) < collsionDistance):
+        return True
+    else:
+        return False
+
+def checkBallBorderCollision(balls):
+    for p in balls:
+        if(checkBorderCollision(p.position[1], topY, 20)):
+            p.unitVector[1] *= -1
+        elif(checkBorderCollision(p.position[1], bottomY, 10)):
+            p.unitVector[1] *= -1
+        elif(checkBorderCollision(p.position[0], leftX, 10)):
+            p.unitVector[0] *= -1
+        elif(checkBorderCollision(p.position[0], rightX, 5)):
+            p.unitVector[0] *= -1
+
+#Main loop
+while Running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            Running = False
+            pygame.quit()
+            quit()
+
+    gameDisplay.fill((0,0,0))
+
+
+    for p in balls:
+        p.displayBall()
+        p.calcSpeed()
+        p.moveBall()
+ 
+    checkBallBorderCollision(balls)
+
+    #Check collision with other balls
+    x = 1
+    for boll in balls:
+        someBalls = balls[x:]
+        for anotherBall in someBalls:
+            checkBallCollision(boll,anotherBall)
+        x += 1
+        if(x == len(balls)):
+            break
+
+
+
+    gameDisplay.blit(borderTop, (topX, topY))
+    gameDisplay.blit(borderBottom, (bottomX, bottomY))
+    gameDisplay.blit(borderLeft, (leftX, leftY))
+    gameDisplay.blit(borderRight, (rightX, rightY))
+
+    pygame.display.update()
